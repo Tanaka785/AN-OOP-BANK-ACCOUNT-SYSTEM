d825f37522f19584bba78ea56414d6053e0bd2d7
Tanaka785 <162094526+Tanaka785@users.noreply.github.com>
2024-07-19 00:27:31 +0200

    imported 'csv' for checking the existence of an account in the csv 'bankaccount.csv'

--- bankaccount.py
+++ bankaccount.py
@@ -1,7 +1,12 @@
+import csv
+
 class Account:
     def __init__(self):
         self.balance = 0
 
+
+    def sign_up_or_sign_in():
+        ...
 
 class Savings_Account(Account):
     def __init__(self):
@@ -15,3 +20,7 @@ class Checkings_Account(Account):
 
 class Credit_Account(Account):
     ...
+
+
+account = Account()
+account.sign_up_or_sign_in()
