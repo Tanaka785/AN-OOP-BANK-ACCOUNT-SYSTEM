3e98fda03296eb15989103709f2aa7fa32ee5083
Tanaka785 <162094526+Tanaka785@users.noreply.github.com>
2024-07-19 00:21:32 +0200

    Also inherited the functionality of the __init__ method of the base class 'Account' for all the derived classes

--- bankaccount.py
+++ bankaccount.py
@@ -4,11 +4,13 @@ class Account:
 
 
 class Savings_Account(Account):
-    ...
+    def __init__(self):
+        super().__init__()
 
 
 class Checkings_Account(Account):
-    ...
+    def __init__(self):
+        super().__init__()
 
 
 class Credit_Account(Account):
