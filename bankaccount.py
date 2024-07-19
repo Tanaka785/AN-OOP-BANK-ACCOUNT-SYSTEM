93a9b4c3580ae8c4192b42b9255b2eaa722a31b5
Tanaka785 <162094526+Tanaka785@users.noreply.github.com>
2024-07-19 09:36:46 +0200

    Added the functionality of getting the user's sign in or up option in the function 'get_signing_in_or_up_options'

--- bankaccount.py
+++ bankaccount.py
@@ -14,13 +14,12 @@ class Account:
         while True:
             self.print_signing_in_or_up_options()
             self.option = self.get_signing_in_or_up_option()
-            if (self.validate_signing_in_or_up_option(self.option)):
-                if (self.option) == 1:
-                    self.sing_up()
-                elif (self.option) == 2:
-                    self.sign_in()
-                else:
-                    self.quit_program()
+            if (self.option) == 1:
+                self.sign_up()
+            elif (self.option) == 2:
+                self.sign_in()
+            else:
+                self.quit_program()
 
 
     def print_signing_in_or_up_options(self):
@@ -32,8 +31,10 @@ class Account:
 
 
     def get_signing_in_or_up_option(self):
-        self.option = input("CHOOSE OPTION: ").strip()
-        return (self.option)
+        while True:
+            self.option = input("CHOOSE OPTION: ").strip()
+            if (self.validate_signing_in_or_up_option(self.option)):
+                return (self.option)
     
 
     def validate_signing_in_or_up_option(self, option):
@@ -42,19 +43,36 @@ class Account:
                 self.option = int(self.option)
             except ValueError:
                 print("Please Enter An Integer Value For The Option!")
+                self.sign_up_or_sign_in()
             else:
                 if (self.option) in (self.signing_in_or_up_options):
                     return True
                 else:
                     print("Please Choose An Option that Is Among The Provided Options!")
+                    self.sign_up_or_sign_in()
         else:
             print("Come on now 😌. Please Enter Something")
-            
+            self.sign_up_or_sign_in()
+
 
     def sign_up(self):
-        ...
+        user_id_number = self.get_user_id_number
 
-    
+
+    def get_user_id_number(self):
+        while True:
+            user_id_number = input("ID Number: ").strip()
+            if self.validate_user_id_number(user_id_number):
+                return user_id_number
+
+
+    def validate_user_id_number(self, user_id_number):
+        if user_id_number:
+            ...
+        else:
+            print("Come on now 😌. Please Enter Something")
+
+
     def sign_in(self):
         ...
 
